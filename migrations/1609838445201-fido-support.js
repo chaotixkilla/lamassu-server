52d086c9cdc6ad15a52f21132dd602199205c183
Sérgio Salgado <sergiodiassalgado@gmail.com>
2021-01-11 12:50:08 +0000

    feat: move attestation to DB

--- migrations/1609838445201-fido-support.js
+++ migrations/1609838445201-fido-support.js
@@ -2,7 +2,8 @@ var db = require('./db')
 
 exports.up = function (next) {
   var sql = [
-    'ALTER TABLE user_register_tokens ADD COLUMN use_fido BOOLEAN DEFAULT false'
+    'ALTER TABLE user_register_tokens ADD COLUMN use_fido BOOLEAN DEFAULT false',
+    'ALTER TABLE users ADD COLUMN hardware_credentials JSONB'
   ]
 
   db.multi(sql, next)
